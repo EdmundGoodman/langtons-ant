78a036e2e77c01670b0d3d225988017ad04fe0e5
EdmundGoodman <37504168+EdmundGoodman@users.noreply.github.com>
2018-06-20 09:50:30 +0100

    Added comments, and changed display output

--- langtonsAnt.py
+++ langtonsAnt.py
@@ -1,16 +1,16 @@
 #Edmund Goodman - Creative Commons Attribution-NonCommercial-ShareAlike 2.5
 #Langton's Ant
-import os, math, colorsys
-from turtle import *
+import os, math
 
 class ant:
-    def __init__(self, direction, position, board, speed):
+    def __init__(self, direction, position, board):
+        #Initialise the member variables
         self.direction = direction
         self.position = position
         self.board = board
-        self.speed = speed
 
     def turn(self, boardColour):
+        #Change the ant's direction based on the current square colour
         if boardColour == 1:
             self.direction -= 1
         else:
@@ -18,9 +18,11 @@ class ant:
         self.direction %= 4
 
     def flip(self, boardColour):
+        #Flip the colour of the current square
         self.board[self.position[1]][self.position[0]] = (boardColour+1)%2
 
     def move(self):
+        #Move the ant 1 square in the current direction
         if self.direction == 0:
             self.position[1] += 1
         elif self.direction == 1:
@@ -31,23 +33,29 @@ class ant:
             self.position[0] -= 1
 
     def displayBoard(self):
-        pu()
-        goto(self.position[0]*5-500,self.position[1]*5-500)
-        pd()
-        dot(5)
+        #Display the board
+        for i,y in enumerate(self.board):
+            for j,x in enumerate(y):
+                if i == self.position[1] and j == self.position[0]:
+                    print(list("^>v<")[self.direction], end="")
+                elif x:
+                    print("\u2588", end="")
+                else:
+                    print(" ", end="")
+            print()
 
-speed(0)
-setup(1000,1000)
-tracer(100,0)
-os.system('clear')
-iterationCount, width, height = 13579, 200, 200
-ant1 = ant(0,[int(width/2),int(height/2)], [[0 for _ in range(height)] for _ in range(width)], 0)
+#Initialise variables
+iterationCount, width, height = 12500, 150, 150
+
+#Initialise the ant
+ant1 = ant(0,[int(width/2),int(height/2)], [[0 for _ in range(height)] for _ in range(width)])
+
 for i in range(iterationCount):
+    #Take one step in the automaton
     boardColour = ant1.board[ant1.position[1]][ant1.position[0]]
     ant1.turn(boardColour)
     ant1.flip(boardColour)
     ant1.move()
-    color(colorsys.hsv_to_rgb(i/1000,1.0,1.0))
-    ant1.displayBoard()
-update()
-done()
+
+#Print the board
+ant1.displayBoard()
